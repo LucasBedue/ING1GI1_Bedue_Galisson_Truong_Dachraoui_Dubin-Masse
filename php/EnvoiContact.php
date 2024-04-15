f95631f8345e05e0e4cd3b037a090029f45d3249
Lastbaguette <114499223+Lastbaguette@users.noreply.github.com>
2024-04-15 15:03:16 +0200

    correction

--- php/EnvoiContact.php
+++ php/EnvoiContact.php
@@ -44,7 +44,7 @@ if(isset($_POST["contactbutton"])){
 		//Recipients
 		
 		$email->setFrom('ganvi2dormir@outlook.fr');
-		$email->addAddress('lucasbedue@gmail.com');
+		$email->addAddress('matthiasgalisson@gmail.com');
 
 		//Content
 		$email->isHTML(true);
